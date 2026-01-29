03ac7760cf84c89570f68659bfc73d8ea7e4d5f9
LSMcIntosh <lamcintosh@ucdavis.edu>
2026-01-29 12:39:51 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,12 @@
+# 10demo by lauren
+
+import math
+
+print ("hello, again")  # greeting
+
+print(1.5e-2)
+print (16//5)
+print (math.log(100)) # this is ln
+print (pow(5, 5)) # gives an integer
+print (math.pow(5, 5)) # gives a float
+
